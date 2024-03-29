37c162f12dd0b2174b63ca1bab75f8e14ef57a49
NaokiNishikawa <naokinishikawa@NaokiNishikawanoMacBook-Air.local>
2024-03-29 12:36:44 +0900

    feature:血液型をajaxを利用して変更できるにようにしました

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do 
+    resource :blood_type, only: %i[update], controller: 'users/blood_type'
+  end
 
   root "users#index"
 end
---
+++ test/controllers/blood_type_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BloodTypeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
